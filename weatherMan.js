9671693590205abcdf6f26d4e0524d3a64c33f70
Tyler Yip <tyleryiptyleryip@gmail.com>
2016-08-10 11:13:05 +0800

    Updated weatherMan file

--- weatherMan.js
+++ weatherMan.js
@@ -3,13 +3,13 @@ var weather = prompt("Welcom to the Weather Man, which is me of course. I would 
 weather = weather.toLowerCase();
 
 if (weather == "sunny") {
-	document.getElementById("animation").scr="//giphy.com/embed/AEy0YJWLIjzSo";
+	document.getElementById("animation").src="//giphy.com/embed/AEy0YJWLIjzSo";
 } else if (weather == "rainy") {
-	document.getElementById("animation").scr="//giphy.com/embed/148x4ezZxvpIeA";
+	document.getElementById("animation").src="//giphy.com/embed/148x4ezZxvpIeA";
 } else if (weather == "windy") {
-	document.getElementById("animation").scr="//giphy.com/embed/Y8PrJpQ2Q91fy";
+	document.getElementById("animation").src="//giphy.com/embed/Y8PrJpQ2Q91fy";
 } else {
 	alert("Try again! This is not a weather I know!")
-	document.getElementById("animation").scr="//giphy.com/embed/LmFG0Z8cnWXqE"
+	document.getElementById("animation").src="//giphy.com/embed/LmFG0Z8cnWXqE"
 }
 
