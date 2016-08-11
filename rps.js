91b5537cdf72a1237772e331ecf8cc4cfcc09dc6
Tyler Yip <tyleryiptyleryip@gmail.com>
2016-08-11 10:56:50 +0800

    inform comp cohice

--- rps.js
+++ rps.js
@@ -12,27 +12,27 @@ function play() {
 
 	computerChoice = options[randomNumber];
 	console.log(computerChoice);
+	alert("The computer's choice is: " + computerChoice);
 
 	if (userChoice == computerChoice) {
-		//document.getElementById("result").innerHTML = "It's a tie!";
-
+		document.getElementById("result").innerHTML = "It's a tie!";
 	} else if (userChoice == "rock" && computerChoice == "scissors") {
-		//document.getElementById("result").innerHTML = "You win!";
+		document.getElementById("result").innerHTML = "You win!";
 		alert("You Win!");
 	} else if (userChoice == "rock" && computerChoice == "paper") {
-		//document.getElementById("result").innerHTML = "computer wins!";
+		document.getElementById("result").innerHTML = "computer wins!";
 		alert("Computer Wins!");
 	} else if (userChoice == "paper" && computerChoice == "rock") {
-		//document.getElementById("result").innerHTML = "You win!";
+		document.getElementById("result").innerHTML = "You win!";
 		alert("You Win!");
 	} else if (userChoice == "paper" && computerChoice == "scissors") {
-		//document.getElementById("result").innerHTML = "computer wins!";
+		document.getElementById("result").innerHTML = "computer wins!";
 		alert("Computer Wins!");
 	} else if (userChoice == "scissors" && computerChoice == "paper") {
-		//document.getElementById("result").innerHTML = "You wins!";
+		document.getElementById("result").innerHTML = "You wins!";
 		alert("You Win!");
 	} else if (userChoice == "scissors" && computerChoice == "rock") {
-		//document.getElementById("result").innerHTML = "computer wins!";
+		document.getElementById("result").innerHTML = "computer wins!";
 		alert("Computer Wins!");
 	} else {
 		alert("Invalid Input!");
